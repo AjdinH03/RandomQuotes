b679943686b5228e09a0b78f8bec883b6bdf73d9
AjdinH03 <ajdinhusetovicc03@gmail.com>
2021-12-12 15:04:28 +0100

    commit

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // array of quotes
 const quotes = [
     "“The purpose of our lives is to be happy.” - Dalai Lama",
-    "“Get busy living or get busy dying.” - John Lennon",
-    "“You only live once, but if you do it right, once is enough.” - Stephen King"
+    "“Get busy living or get busy dying.” - Stephen King",
+    "“You only live once, but if you do it right, once is enough.” - Mae West",
+    "“Many of life’s failures are people who did not realize how close they were to success when they gave up.” – Thomas A. Edison",
+    "“If you want to live a happy life, tie it to a goal, not to people or things.” – Albert Einstein",
+    "“Never let the fear of striking out keep you from playing the game.” – Babe Ruth",
 ]
 
 const btnGen = document.querySelector(".generator");
@@ -14,3 +17,4 @@ btnGen.addEventListener('click', function(){
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
     quote.innerHTML = randomQuote;
 });
+
